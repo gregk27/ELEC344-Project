46c613f396567dfca77e8f8fd97142b8c446865e
gregk27 <git@gregk.ca>
2021-11-19 17:57:36 -0500

    Create basic webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,38 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <SPIFFS.h>
+#include <gyro.h>
+
+const char* ssid = "ELEC344";
+const char* password = "password";
+
+WebServer server(80);
+
+void handle_getPage();
+void handle_getRaw();
 
 void setup(){
     Serial.begin(9600);
     Serial.println("Hello World");
+
+    WiFi.softAP(ssid, password);
+    Serial.print("Created newtork, IP: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", handle_getPage);
+    server.on("/raw", handle_getRaw);
+
+    server.begin();
 }
 
 void loop(){
+    server.handleClient();
+}
+
+void handle_getRaw(){
+}
+
+void handle_getPage(){
+
 }
